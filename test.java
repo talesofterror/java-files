3c6984d752801c0e42e251a72e67df8b32a85b7c
mike <mich.rivera@gmail.com>
2021-10-29 11:32:02 -0400

    Fucked around with the scanner. Can't figure out why an error is thrown when I close the scanner.

--- test.java
+++ test.java
@@ -1,31 +1,41 @@
-import java.io.Console;
 import java.util.Scanner;
 
 public class test {     
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         start();
-
+    
     }
 
     public static void start() {
 
-        System.out.println("hello world");
-
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Advanced Sum Solver (A.S.S.) v0.01");
+        
+        
+        System.out.println("Enter first integer");
         var input = scanner.next();
+        System.out.println("Enter second integer");
         var input2 = scanner.next();
+        scanner.reset();
+        // scanner.close(); using this throws an error: Exception in thread "main" java.lang.IllegalStateException: Scanner closed
 
-        System.out.println(Integer.parseInt(input) + Integer.parseInt(input2));
+        /*
+        This may be due to me defining scanner as Scanner(System.in). Maybe if I create
+        an object containing a new System.in stream each time the program runs the error will
+        go away???
+        */
 
-        scanner.close();
+        System.out.println(input + " + " + input2 + " = " + Integer.parseInt(input) + Integer.parseInt(input2));
 
         restart();
 
     }
 
     public static void restart () {
-        System.out.println("Restarting");
+        System.out.println("Restarting...");
+
         start();
     }
 
